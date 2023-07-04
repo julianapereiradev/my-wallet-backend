cf83ad8f92961c70067979f52f3aa2fdab282383
juliana <julianapereira.dev@gmail.com>
2023-07-04 14:28:35 -0300

    Implement participants POST and GET routes (cadastro)

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { MongoClient, ObjectId } from "mongodb";
+import joi from 'joi'
 
+//Settings:
 const app = express();
 
 app.use(cors());
@@ -17,13 +19,71 @@ try {
 } catch (err) {
   (err) => console.log(err.message);
 }
-
 const db = mongoClient.db();
 
-//Endpoints:
-app.get("/teste", (req, res) => {
-  res.send("Teste funcionando!");
+
+//Joi Schemas:
+const participantSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+    passwordagain: joi.string().required(),
 });
+
+
+//Cadastro:
+app.post("/participants", async (req, res) => {
+    const { name, email, password, passwordagain} = req.body;
+  
+    const postParticipant = { name: name, email: email, password: password, passwordagain: passwordagain }
+  
+    const validation = participantSchema.validate(postParticipant, { abortEarly: false });
+  
+    if (validation.error) {
+      const errors = validation.error.details.map((detail) => detail.message);
+      return res.status(422).send(errors);
+    }
+
+    if(password !== passwordagain) {
+        return res.status(422).send("As senhas estão diferentes")
+    }
+  
+    try {
+    const participantEmailExistsInParticipants = await db.collection("participants").findOne({ email });
+  
+    if(participantEmailExistsInParticipants) {
+      return res.status(409).send("Este email já existe no banco");
+    }
+    
+      const newParticipant = { name: name, email: email, password: password };
+      
+      await db.collection("participants").insertOne(newParticipant);
+      res.sendStatus(201)
+  
+  } catch(err) {
+      return res.status(500).send(err.message)
+  }
+  });
+
+app.get("/participants", async (req, res) => {
+    try {
+      const data = await db.collection("participants").find().toArray();
+      return res.send(data);
+  
+    } catch (err) {
+      return res.status(500).send(err.message);
+    }
+  });
+
+
+  //Login:
+  app.post("/user", async (req, res) => {
+    try {
+  
+    } catch (err) {
+      return res.status(500).send(err.message);
+    }
+  });
 
 //PORT:
 const PORT = 5000;
