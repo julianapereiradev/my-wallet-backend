cf9787e7f0ce14bac71ddbf29888e2a63ee2bc68
juliana <julianapereira.dev@gmail.com>
2023-07-04 14:00:07 -0300

    Mongo, Express, Dotenv and PORT settings

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient, ObjectId } from "mongodb";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+
+try {
+  await mongoClient.connect();
+  console.log("MongoDB conectado!");
+} catch (err) {
+  (err) => console.log(err.message);
+}
+
+const db = mongoClient.db();
+
+//Endpoints:
+app.get("/teste", (req, res) => {
+  res.send("Teste funcionando!");
+});
+
+//PORT:
+const PORT = 5000;
+app.listen(PORT, () =>
+  console.log(`O servidor está rodando na porta ${PORT}!`)
+);
